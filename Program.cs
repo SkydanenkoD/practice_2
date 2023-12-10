181b3511c4dfad1fcf0d46ba929a0dc0e3af6653
Denys Skydanenko <d.d.skydanenko@student.khai.edu>
2023-12-11 00:12:01 +0200

    Query1

--- Program.cs
+++ Program.cs
@@ -47,15 +47,17 @@ namespace Practice_Linq
         static void Query1(List<FootballGame> games)
         {
             //Query 1: Вивести всі матчі, які відбулися в Україні у 2012 році.
-
-            var selectedGames = games; // Корегуємо запит !!!
+            var selectedGames = games.OrderBy(game => game.Date).Where(game => (game.Date.Year == 2012 && game.Country == "Ukraine"));
 
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 1 ========================");
 
             // див. приклад як має бути виведено:
-
+            foreach (var game in selectedGames)
+            {
+                Console.WriteLine($"{game.Date:dd.MM.yyyy} {game.Home_team} - {game.Away_team}, Score: {game.Home_score} - {game.Away_score}, Country: {game.Country}");
+            }
 
         }
 
